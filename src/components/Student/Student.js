46cfed6c77db213baac1a968dda2dec854841ec1
Karen - iMac <kellsworth628@gmail.com>
2021-03-22 16:34:20 -0600

    completed WestSide Univ Lab

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { HashRouter, Link } from 'react-router-dom';
+
+
 
 export default class Student extends Component {
   constructor() {
-    super()
+    super();
+
+    this.state = {
+      studentInfo: {}
+    };
 
   }
 
+  componentDidMount() {
+    return axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(results => {
+        this.setState({
+          studentInfo: results.data
+        });
+      });
+  }
+
+
   render() {
     return (
-      <div className="box">
-        <h1>Student</h1>
-      </div>
-    )
+      <HashRouter>
+        <div className="box">
+          <h1>Student</h1>
+          <h1>
+            {this.state.studentInfo.first_name} {this.state.studentInfo.last_name}
+          </h1>
+          <h3>Grade: {this.state.studentInfo.grade}</h3>
+          <h3>Email: {this.state.studentInfo.email}</h3>
+          <div>
+            <Link to="/classlist/MATH1010" className='MATH1010'>Back</Link>
+          </div>
+          <div>
+            <Link to="/classlist/ENG2010" className='ENG2010'>Back</Link>
+          </div>
+          <div>
+            <Link to="/classlist/BIO2020" className='BIO2020'>Back</Link>
+          </div>
+        </div >
+      </HashRouter>
+    );
   }
 }
